9de2614b97c4f18700ac69d43036b9b1abf80537
Hashitumo <43104226+Tripzin@users.noreply.github.com>
2019-06-09 13:12:49 +0900

    Update teikou.js

--- docs/teikou/script/teikou.js
+++ docs/teikou/script/teikou.js
@@ -1,5 +1,5 @@
 $(function(){
-
+/*
     var win_w = window.innerWidth;
     var win_h = window.innerHeight;
     var aspect = win_w/win_h;
@@ -13,6 +13,10 @@ $(function(){
         $('.all').css("height","17%");
         $(".buttons > div").css("height","20%");
     }
+*/    
+
+
+
 
     function calom(nums){
         var diti = parseInt(nums[0]-1);
@@ -46,16 +50,16 @@ $(function(){
                 dyon = 20;
         }
 
-        return String(Math.floor((diti*10+dni)*10**dsan*10)/10)+'Ω 誤差±'+dyon+"%";
+        return String(Math.floor((diti*10+dni)*10**dsan*10)/10)+'Ω ±'+dyon+"%";
     }
     /**ドラムロールの設定 */
     var num_list = [];
     var color_list_ty0 = ['black','brown','red','orange','yellow',
-                      'green','blue','magenta','gray','white'];
+                      'green','#0e72cf','magenta','gray','white'];
     var color_list_ty1 = ['brown','red','gold','silver','snow'];
 
     var color_list_ty2 = ['black','brown','red','orange','yellow',
-    'green','blue','gold','silver'];
+    'green','#0e72cf','gold','silver'];
 
     $('.dram0').children('li').each(function(index){
         $(this).css('background',color_list_ty0[index]);
